adc413d7b17c6f9320244fc98112bc67d665e398
taki hasan <takihasan@takis-MacBook-Air.local>
2024-01-28 15:25:11 +0530

    look

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,8 +29,7 @@ def load_db(file, chain_type, k):
       vectordb.as_retriever(search_type = 'similarity',  
                             search_kwargs = {"k": k}), 
       chain_type = chain_type,
-      return_source_documents = True, 
-      return_generated_question= True
+      return_source_documents = True
   )
 
   return qa
